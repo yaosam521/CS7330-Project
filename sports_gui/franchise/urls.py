21f3a72b7f0653494b83a2df7e18bf7f9d9a81c9
yaosam521 <yaosam.521@gmail.com>
2022-11-28 05:55:23 -0600

    Created boilerplate for three query views

--- sports_gui/franchise/urls.py
+++ sports_gui/franchise/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('league/', views.league, name = 'league'),
     path('date/', views.date, name = 'date'),
-    path('season/', views.season, name = 'season')
+    path('season/', views.season, name = 'season'),
+    path('league_query/',views.league_query, name='league_query'),
+    path('team_query/',views.team_query, name='team_query'),
+    path('game_query/',views.game_query, name='game_query'),
+    path('results/',views.results, name='results'),
+    path('move_teams/',views.move_teams, name='move_teams')
 ]
--- sports_gui/franchise/views.py
+++ sports_gui/franchise/views.py
@@ -18,3 +18,23 @@ def date(request):
 def season(request):
     template = loader.get_template('season/season.html')
     return HttpResponse(template.render())
+
+def league_query(request):
+    template = loader.get_template('queries/league_query.html')
+    return HttpResponse(template.render())
+
+def team_query(request):
+    template = loader.get_template('queries/team_query.html')
+    return HttpResponse(template.render())
+
+def game_query(request):
+    template = loader.get_template('queries/game_query.html')
+    return HttpResponse(template.render())
+
+def results(request):
+    template = loader.get_template('results.html')
+    return HttpResponse(template.render())
+
+def move_teams(request):
+    template = loader.get_template('move_teams.html')
+    return HttpResponse(template.render())
