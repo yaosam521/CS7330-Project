7a9ebb974245f1ca5c03ef5fa8fc48c0709b7455
Zinoope <brahmi.medzineeddine@gmail.com>
2022-11-29 00:41:16 -0600

    after poping the stash

--- back_end/Project7330.py
+++ back_end/Project7330.py
@@ -12,7 +12,7 @@ inTeams=[{"tName":"A","City": "testcity", "State": "testState", "Field": "A Home
 {"tName":"E","City": "testcity2", "State": "testState2", "Rating": -34211.4},
 {"tName":"F","City": "testcity2", "State": "testState2", "Rating": -34211.4}]
 
-inSeasons={"lName": "test League", "sDate": "2020-01-01","eDate": "2020-06-20", "gNumber": 2, "sRules": {"win":3, "draw": 1, "lose":0}}
+inSeasons={"lName": "test League2", "sDate": "2020-01-01","eDate": "2020-06-20", "gNumber": 2, "sRules": {"win":3, "draw": 1, "lose":0}}
 
 inGames=[{"Record":{"A": None, "B": None}, "Field": "testfield", "Date":"2020-01-01"},
 		 {"Record":{"C": None, "D": None}, "Field": "testfield", "Date":"2020-01-01"},
@@ -21,20 +21,24 @@ inGames=[{"Record":{"A": None, "B": None}, "Field": "testfield", "Date":"2020-01
 
 autoInsertion= True
 
+init_date()
+	
 
-insert_league(inLeagues)
+
+insert_league(inLeagues2)
 for team in inTeams:
 	insert_team(team)
 insert_season(inSeasons, autoInsertion, maxPerDay= 3, inGames=inGames)
 insert_game_res("E", -12, "D", 0, "2020-01-01", t1Rating=-34211.4, replace=True)
-move_team("A", "test League", "test League2", "2020-07-23")
+move_team("A", "test League2", "test League", "2020-07-23")
 season_query("test League", "2020-01-01", "2020-06-20")
 
 game_query("c", "B")
 
-team_info_query("A")
+team_info_query("c")
 
-team_records_query("A")
+
+team_records_query("E")
 league_info_query("test League")
 league_champians_query("test League2")
 rating_query("test League","2020-01-01","2020-06-20")
