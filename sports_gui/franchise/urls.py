aa0ada5a33fc95bb100bf324a398d221d071a3c4
Zinoope <brahmi.medzineeddine@gmail.com>
2022-11-29 11:44:05 -0600

    solve the cache files conflicts

--- sports_gui/franchise/urls.py
+++ sports_gui/franchise/urls.py
@@ -4,5 +4,15 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('league/', views.league, name = 'league')
+    path('league/', views.league, name = 'league'),
+    path('date/', views.date, name = 'date'),
+    path('season/', views.season, name = 'season'),
+    path('league_query/',views.league_query, name='league_query'),
+    path('team_query/',views.team_query, name='team_query'),
+    path('game_query/',views.game_query, name='game_query'),
+    path('ratings_query/',views.ratings_query, name='ratings_query'),
+    path('season_query/',views.season_query, name='season_query'),
+    path('results/',views.results, name='results'),
+    path('move_teams/',views.move_teams, name='move_teams'),
+    path('manual_insert_teams/',views.manual_insert_teams, name='manual_insert_teams')
 ]
--- sports_gui/franchise/views.py
+++ sports_gui/franchise/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 
 def index(request):
@@ -8,4 +9,44 @@ def index(request):
 
 def league(request):
     template = loader.get_template('league/league.html')
+    return HttpResponse(template.render())
+
+def date(request):
+    template = loader.get_template('date/date.html')
+    return HttpResponse(template.render())
+
+def season(request):
+    template = loader.get_template('season/season.html')
+    return HttpResponse(template.render())
+
+def league_query(request):
+    template = loader.get_template('queries/league_query.html')
+    return HttpResponse(template.render())
+
+def team_query(request):
+    template = loader.get_template('queries/team_query.html')
+    return HttpResponse(template.render())
+
+def game_query(request):
+    template = loader.get_template('queries/game_query.html')
+    return HttpResponse(template.render())
+
+def ratings_query(request):
+    template = loader.get_template('queries/ratings_query.html')
+    return HttpResponse(template.render())
+
+def season_query(request):
+    template = loader.get_template('queries/season_query.html')
+    return HttpResponse(template.render())
+
+def results(request):
+    template = loader.get_template('results.html')
+    return HttpResponse(template.render())
+
+def move_teams(request):
+    template = loader.get_template('move_teams.html')
+    return HttpResponse(template.render())
+
+def manual_insert_teams(request):
+    template = loader.get_template('manual_insert_teams.html')
     return HttpResponse(template.render())
