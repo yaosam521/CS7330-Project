f52df4e6bde8aa0f5959bee06589b391f5ce98dc
yaosam521 <yaosam.521@gmail.com>
2022-11-29 15:04:42 -0600

    created insert_games view

--- sports_gui/franchise/urls.py
+++ sports_gui/franchise/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     path('move_teams/',views.move_teams, name='move_teams'),
     path('teamEntry/', views.teamEntry, name = 'teamEntry'),
     path('resultsEntered/', views.resultsEntered, name = 'resultsEntered'),
+    path('insert_games/',views.insert_games, name='insert_games'),
     path('manual_insert_teams/',views.manual_insert_teams, name='manual_insert_teams')
 ]
